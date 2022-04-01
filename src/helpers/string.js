765e03de9d1ac3c417963c2f95a7dcd0025a03a8
jaggs07 <jagaran@fusemachines.com>
2022-04-01 11:57:57 +0545

    Remove console

--- src/helpers/string.js
+++ src/helpers/string.js
@@ -1,5 +1,4 @@
 export function getInitials (fullName) {
-  console.log({ fullName })
   let [ firstName = '', lastName = '' ] = fullName.split(' ');
   return `${firstName.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}`
 }
